a76683ae14a141cd73c1a5af57681a317d5449c3
owendanke <dankeowen@gmail.com>
2023-11-02 16:59:44 -0400

    Refactor and additions to code base

--- Networking/clientserver/client.java
+++ Networking/clientserver/client.java
@@ -1,2 +1,41 @@
+import java.io.*;
+import java.net.Socket;
+
 public class client {
+    String serverIP = "localhost";
+    int Port = 12890;
+
+    public void writeData(Socket socket) throws IOException {
+        BufferedReader reader = new BufferedReader(new InputStreamReader(System.in));
+        PrintWriter toServer = new PrintWriter(socket.getOutputStream(), true);
+        String input = reader.readLine();
+        do{
+            toServer.println(input);
+        } while(!reader.readLine().equals("DONE"));
+
+    }
+
+    public void readData(){
+
+    }
+
+    public String getIP(){
+        return serverIP;
+    }
+
+    public int getPort(){
+        return Port;
+    }
+
+    public static void main(String[] args){
+        client client = new client();
+        try {
+            Socket socket = new Socket(client.getIP(), client.getPort());
+            client.writeData(socket);
+        } catch (IOException ioe){
+            System.err.println("Error opening socket");
+        }
+
+    }
+
 }
--- Networking/clientserver/threadedServer.java
+++ Networking/clientserver/threadedServer.java
@@ -4,58 +4,53 @@ import java.net.Socket;
 
 public class threadedServer implements Runnable {
     int PORT = 12890;
-    int MAXTHREADS;
-    Socket socket;
+    static int MAXTHREADS = 4; //use up to 4 threads for clients
+    ServerSocket socket;
+    Socket clientSocket;
     String Username;
     String data;
-    DataInputStream inputStream;
-    DataOutputStream outputStream;
 
-    public threadedServer(Socket socket, String username){
-        this.socket = socket;
+    public threadedServer(Socket clientSocket, String username){
+        this.clientSocket = clientSocket;
         this.Username = username;
     }
 
-    public void setup() {
-        try {
-            ServerSocket serverSkt = new ServerSocket(PORT);
-        } catch (IOException ioe){
-            System.err.println("Error opening port");
-        } catch (IllegalArgumentException IAE){
-            System.err.println("Specified port outside of range (0-65535)");
-        }
+    public threadedServer(){
+
     }
 
-    public void start(){
+    public void startServer(){
         try {
-            ServerSocket srvSkt = new ServerSocket(PORT);
-            Socket client1 = srvSkt.accept();
-            Socket client2 = srvSkt.accept();
 
-            Thread srv1 = new Thread(new threadedServer(client1, "client1"));
-            Thread srv2 = new Thread(new threadedServer(client2, "client2"));
+            //currently just do 2 clients for now
 
+            Thread srv1 = new Thread(new threadedServer());
+            Thread srv2 = new Thread(new threadedServer());
 
             srv1.start();
             System.out.println("Started client 1 thread");
             srv2.start();
             System.out.println("Started client 2 thread");
+
+            /*
+
+            for(int i=0, i < MAXTHREADS
+
+             */
         } catch (IllegalArgumentException IAE){
             System.err.println("Specified port outside of range (0-65535)");
-        } catch (IOException ioe){
-            System.err.println("Exception with sockets");
-        }
+        } //catch (IOException ioe){
+          //  System.err.println("Exception with sockets");
+        //}
     }
 
     public void writeData(String data){
         //write to outputStream
     }
 
-    public void readData(){
-        BufferedReader bufferedReader = new BufferedReader(new InputStreamReader(inputStream));
-
-
-
+    public String readData() throws IOException {
+        BufferedReader fromServer = new BufferedReader(new InputStreamReader(clientSocket.getInputStream()));
+        return fromServer.readLine();
     }
 
     public String getData(){
@@ -69,14 +64,29 @@ public class threadedServer implements Runnable {
     @Override
     public void run() {
         //create a new server object which opens and accepts a client's connection
+        while(true){
+            try {
+                data = readData();
+                if(!data.equals("DONE")){
+                    System.out.println(data);
+                }
+                else {
+                    close();
+                    break;
+                }
+            } catch (IOException e) {
+                System.err.println("Error reading data from client");
+            }
 
-
+        }
     }
 
-    public static void main(String[] args){
-
-
-
+    public void close(){
+        try {
+            socket.close();
+        } catch (IOException ioe){
+            System.err.println("Couldn't close socket");
+        }
     }
 
 }
